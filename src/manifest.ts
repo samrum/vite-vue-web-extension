96c417651c7e667c5956a26340a5b294f5ed4d44
Ruben Medina <dev@rubenmedina.com>
2022-01-15 16:28:46 -0800

    feat: add additional icon sizes

--- src/manifest.ts
+++ src/manifest.ts
@@ -15,10 +15,15 @@ const sharedManifest = {
   description: pkg.description,
   icons: {
     16: "icons/16.png",
+    19: "icons/19.png",
     32: "icons/32.png",
+    38: "icons/38.png",
     48: "icons/48.png",
     64: "icons/64.png",
+    96: "icons/96.png",
     128: "icons/128.png",
+    256: "icons/256.png",
+    512: "icons/512.png",
   },
   name: pkg.displayName ?? pkg.name,
   options_ui: {
@@ -31,7 +36,9 @@ const sharedManifest = {
 
 const browserAction = {
   default_icon: {
+    16: "icons/16.png",
     19: "icons/19.png",
+    32: "icons/32.png",
     38: "icons/38.png",
   },
   default_popup: "src/popup/index.html",
